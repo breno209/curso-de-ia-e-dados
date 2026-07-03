04e83c373cb376dad9122ccfb269436da68193c2
breno209 <brenomineli10@gmail.com>
2026-07-03 20:42:26 -0300

    adicionando aulas de dados - pandas, numpy- seaborn

---
+++ app.py
@@ -0,0 +1,11 @@
+#comando para exibir informações na tela
+print("boa noite!")
+idade = 30
+idade_dois = "30"
+print(type(idade))
+print(type(idade_dois))
+idade_tres = 30.5
+print(idade)
+print(idade_dois)
+print(type(idade_tres))
+     
